98ed624b68cbb155f422a0c527f0b2a0a59abe39
LUCY-LT\Nick <nickky23@hotmail.com>
2018-10-23 03:46:59 +0700

    enable cors

--- src/routes.js
+++ src/routes.js
@@ -5,26 +5,41 @@ module.exports =  [
     {
         method: 'GET',
         path: '/portfoilo/{user?}',
+        config: {
+            cors : true,
+        },
         handler: AssetController.getAsset
     },
     {
         method: 'POST',
         path: '/portfoilo/buy/{user?}',
+        config: {
+            cors : true,
+        },
         handler: AssetController.buyAsset
     },
     {
         method: 'POST',
         path: '/portfoilo/sell/{user?}',
+        config: {
+            cors : true,
+        },
         handler: AssetController.sellAsset
     },
     {
         method: 'POST',
         path: '/portfoilo/dividend{user?}',
+        config: {
+            cors : true,
+        },
         handler: AssetController.assetDividend
     },
     {
         method: 'POST',
         path: '/portfoilo/update/{user?}',
+        config: {
+            cors : true,
+        },
         handler: AssetController.updateAsset
     },
 ];
