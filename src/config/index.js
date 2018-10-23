bc88aeca9d84454252af3b3a7883b9441da3df9a
LUCY-LT\Nick <nickky23@hotmail.com>
2018-10-23 22:42:00 +0700

    try to fix cors issue

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,8 @@ module.exports = function() {
     var configObj = {
         // Host Server Setting
         application: {
-            port: process.env.PORT ||80
+            port: process.env.PORT ||80,
+            routes: { cors: true }
         }
     };
     return configObj;
