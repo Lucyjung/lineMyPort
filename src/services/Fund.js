e1a6f353163947ce3eb6731fc71939ac4f02fc24
Narut <narut.tr@pttdigital.com>
2019-08-15 09:22:03 +0700

    Fix fund API

--- src/services/Fund.js
+++ src/services/Fund.js
@@ -1,5 +1,5 @@
 // Fund Service
-const FILE_NAME = 'fund.csv';
+const axios = require('axios');
 module.exports = {
 
     getPrice: async (symbols) => {
@@ -10,7 +10,7 @@ module.exports = {
         else{
             for (let i = 0; i < LIMIT_RETRIES; i++){
                 try{
-                    let price_list = await getFundNav(symbols,i);
+                    let price_list = await getFund(symbols,i);
                     if (price_list && Object.keys(price_list).length > 0){
                         return price_list;
                     }
@@ -22,46 +22,42 @@ module.exports = {
         
     }
 };
-
-function getFundNav(symbols, subtract_day){
-
-    return new Promise((fulfilled) => { 
-        let fs = require('fs');
-
-        let http = require('http');
-        let file = fs.createWriteStream(FILE_NAME);
-
-        if (!subtract_day) subtract_day = 0;
-        let moment = require('moment');
-        let d= moment().subtract(subtract_day, 'days').add(543,'year').format('DD/MM/YYYY');
+async function getFund(symbols, subtract_day) {
+    if (!subtract_day) subtract_day = 0;
+    const moment = require('moment');
+    let d= moment().subtract(subtract_day, 'days').add(543,'year').format('DD/MM/YYYY');
+    let fundData = {};
+    const response = await axios.get('https://www.thaimutualfund.com/AIMC/aimc_navCenterDownloadRepRep.jsp?date=' + d);
+    if (response && response.data){
+        let data = response.data.split('\r\n');
         
-        http.get('http://www.thaimutualfund.com/AIMC/aimc_navCenterDownloadRepRep.jsp?date='+d, function(response){
-            let stream = response.pipe(file);
-            stream.on('finish',  ()=> { 
-                const readline = require('readline');
-                const rl = readline.createInterface({
-                    input: fs.createReadStream(FILE_NAME),
-                    crlfDelay: Infinity
-                });
-                let result = {};
-                rl.on('line', (line) => {
-                    
-                    if (line != ''){
-                        line = line.replace(new RegExp('"', 'g'), '');
-                        let dataArr = line.split(',');
-                        if (symbols.indexOf(dataArr[6]) > -1){
-                            result[dataArr[6]] = dataArr[8];
-                        }
-                    }
-                }).on('close', () => {
-                    fulfilled(result);
-                });
-            });
-            
+        for (let i in data){
+            if (data[i] && data[i] != ''){
+                let tmpData = data[i].split(',');
+                if (tmpData.length > 1){
+                    fundData = searchFundNav(symbols, tmpData);
+                }
+            }
+        }
+    }
+    return fundData;    
+}
+function searchFundNav(symbols, fundRawArr){
+    let result = {};
+    const ROW_SIZE = 12;
+    let colSize = Math.floor(fundRawArr.length / ROW_SIZE);
+    for (let col = 0 ; col < colSize; col++){
+        if (!fundRawArr[(col * ROW_SIZE) + 6]){
+            break;
+        }
+        let fundName = fundRawArr[(col * ROW_SIZE) + 6];
+        fundName = fundName.replace(new RegExp('"', 'g'), '');
+        if (symbols.indexOf(fundName) > -1){
+            let nav = fundRawArr[(col * ROW_SIZE) + 8];
+            nav = nav.replace(new RegExp('"', 'g'), '');
+            result[fundName] = nav;
+        }
 
-        });
-         
-        
-    });
-    
+    }
+    return result ;
 }
