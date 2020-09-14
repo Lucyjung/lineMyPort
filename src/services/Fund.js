55fdb2f31eb7dd8ef048d125b2a42c225eb6fcb5
narut.tr <narut.tr>
2020-09-14 14:45:24 +0700

    add double call for fund nav

--- src/services/Fund.js
+++ src/services/Fund.js
@@ -27,24 +27,34 @@ async function getFundAPI(symbols, subtract_day) {
     const moment = require('moment');
     let d = moment().subtract(subtract_day, 'days').format('DD/MM/YYYY');
     let postData = {"amcId":"All","investmentPolicy":"All","dividendPolicy":"All","change":"All","projectType":"All"}
+
     postData.symbols = symbols
     postData.date = d
     let fundData = {};
+    let remnant = [];
     const response = await axios.post('https://api.settrade.com/api/fund-nav/by-condition', postData);
     if (response && response.data && response.data.fundNavs){
         let funds = response.data.fundNavs
         for (let fund of funds){
             fundData[fund.symbol] = fund.navPerUnit
         }
-        
+    }
+
+    for (let symbol of symbols){
+        if (!fundData[symbol]){
+            remnant.push(symbol)
+        }
+    }
+
+    if (remnant.length > 0 ){
+        fundData = getFund(remnant,subtract_day, fundData)
     }
     return fundData;    
 }
-async function getFund(symbols, subtract_day) {
+async function getFund(symbols, subtract_day, fundData) {
     if (!subtract_day) subtract_day = 0;
     const moment = require('moment');
     let d= moment().subtract(subtract_day, 'days').add(543,'year').format('DD/MM/YYYY');
-    let fundData = {};
     const response = await axios.get('https://www.thaimutualfund.com/AIMC/aimc_navCenterDownloadRepRep.jsp?date=' + d);
     if (response && response.data){
         let data = response.data.split('\n');
