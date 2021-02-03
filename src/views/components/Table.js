5ab7214ed896e435472948f6c1d4ca21d73441e2
narut.tr <narut.tr>
2021-02-03 16:42:56 +0700

    update table expander

--- src/views/components/Table.js
+++ src/views/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link} from 'react-router-dom';
 import ReactTable from 'react-table';
 import PropTypes from 'prop-types'; // ES6
+import {isMobile} from 'react-device-detect';
 
 import '../styles/style.css';
 class Table extends Component{
@@ -20,15 +21,19 @@ class Table extends Component{
         },  {
             Header: '#Year',
             accessor: 'holdingYear',
+            show : !isMobile
         },{
             Header: 'Actual Cost',
             accessor: 'cost',
+            show : !isMobile
         },{
             Header: 'Average Cost',
             accessor: 'totalAvgCost',
+            show : !isMobile
         }, {
             Header: 'Market Value',
             accessor: 'marketValue',
+            show : !isMobile
         }, {
             Header: 'Average Cost/Share',
             accessor: 'avgCost',
@@ -45,19 +50,53 @@ class Table extends Component{
         ,{
             Header: 'Avg P&L',
             accessor: 'avgPL',
+            show : !isMobile,
             Cell: props => <span style={{
                 color: props.value >= 0 ? props.value == 0?'#FFFF00': '#57d500': '#ff2e00'
             }}>{props.value} % </span>// Custom cell components!
         }, {
             Header: 'Action',
             Cell: props => <button className='btn btn-lg btn-tran' onClick={(e) => this.props.onAction(e, props)}><i className="fas fa-edit"></i></button> // Custom cell components!
-        }];
+        },
+        {
+            expander: true,
+            Header: 'More',
+            Expander: ({ isExpanded }) =>
+            <button className='btn btn-lg btn-tran'>
+                {isExpanded
+                ? <i class="fas fa-minus"></i>
+                : <i class="fas fa-plus"></i>}
+            </button>
+        }
+    ];
         return (
             <ReactTable
                 data={this.props.data}
                 columns={columns}
                 defaultPageSize={35}
                 pageSize={this.props.data.length || 35}
+                SubComponent={row => {
+                    console.log(row)
+                    let infoTbl = [
+                        {name : 'ROI' , accessor : 'ROI', unit : '%' , style : {color : '#57d500'}},
+                        {name : '#Year' , accessor : 'holdingYear', unit : 'Years' , style : {color : '#FFFFFF'}},
+                        {name : 'Average Dividend Per Year' , accessor : 'avgDividendPerYear', unit : '฿' , style : {color : '#57d500'}},
+                        {name : 'Average Dividend' , accessor : 'avgDividendPercent', unit : '%' , style : {color : '#57d500'}},
+                        {name : 'Current Year Dividend' , accessor : 'curDividend', unit : '฿' , style : {color : '#57d500'}},
+                        {name : 'Current Year Dividend' , accessor : 'curDividendPercent', unit : '%' , style : {color : '#57d500'}},
+                        {name : 'Last Year Dividend' , accessor : 'prevDividend', unit : '฿' , style : {color : '#57d500'}},
+                        {name : 'Last Year Dividend' , accessor : 'prevDividendPercent', unit : '%' , style : {color : '#57d500'}},
+                        {name : 'Actual Cost' , accessor : 'cost', unit : '฿' , style : {color : '#FFFF00'}},
+                        {name : 'Average Cost' , accessor : 'totalAvgCost', unit : '฿' , style : {color : '#FFFF00'}},
+                        {name : 'Market Value' , accessor : 'marketValue', unit : '฿' , style : {color : '#FFFFFF'}},
+                        {name : 'Average P&L' , accessor : 'avgPL', unit : '%' , style : {color : '#57d500'}},
+                    ];
+                    return (
+                        infoTbl.map( (info) =>{
+                            return (<span style={info.style}> {info.name} : {row.original[info.accessor]} {info.unit}</span>)
+                        })
+                    )
+                  }}
             />
         );
     }
