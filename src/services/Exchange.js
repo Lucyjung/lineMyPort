2760ad2bdce8fc0b37683eade0878a99b97240c3
narut.tr <narut.tr>
2021-04-02 23:28:06 +0700

    change exchange

--- src/services/Exchange.js
+++ src/services/Exchange.js
@@ -8,10 +8,14 @@ module.exports = {
         }
         else{
             let result = {};
+            let response = await axios.get('https://openexchangerates.org/api/latest.json?app_id=e6dce8e61b9a4ddf97dee3bc5473af3f');
             for (let symbol of symbols){
-                let response = await axios.get('https://free.currconv.com/api/v7/convert?q=' + symbol + '_THB&compact=ultra&apiKey=6f00b379453f8372bc23');
-                if (response && response.data && response.data[symbol + '_THB']){
-                    result[symbol] = response.data[symbol + '_THB']
+                if (response && response.data){
+                    if (symbol == 'USD'){
+                        result[symbol] = response.data.rates['THB'];
+                    } else{
+                        result[symbol] = response.data.rates['THB']/response.data.rates[symbol];
+                    }
                 }
             }
             return result;
